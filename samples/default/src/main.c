e946f35700a462edfb6d11e2e97f402e146cbe06
alirezaarzehgar <email@alirezaarzehgar.com>
2021-09-04 00:14:43 +0430

    clean up

--- samples/default/src/main.c
+++ samples/default/src/main.c
@@ -4,9 +4,9 @@
  * @brief main file for doing anything
  * @version 0.1
  * @date 2021-09-03
- * 
+ *
  * @copyright Copyright (c) 2021
- * 
+ *
  */
 
 #include <stdio.h>
@@ -15,13 +15,14 @@
 
 /**
  * @brief endpoint
- * 
- * @param argc 
- * @param argv 
- * @return int 
+ *
+ * @param argc
+ * @param argv
+ * @return int
  */
-int main(int argc, char const *argv[])
+int
+main (int argc, char const *argv[])
 {
-    printf("Hello world\n");
-    return EXIT_SUCCESS;
+  printf ("Hello world\n");
+  return EXIT_SUCCESS;
 }
