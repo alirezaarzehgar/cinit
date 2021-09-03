6d1783361ec2c0a4990b11d17c04de209907ac89
alirezaarzehgar <email@alirezaarzehgar.com>
2021-09-04 00:12:03 +0430

    clean up

--- samples/client-server/tcp/client/main.c
+++ samples/client-server/tcp/client/main.c
@@ -4,22 +4,23 @@
  * @brief main file for doing anything
  * @version 0.1
  * @date 2021-09-03
- * 
+ *
  * @copyright Copyright (c) 2021
- * 
+ *
  */
 
 #include "common/common.h"
 
 /**
  * @brief endpoint
- * 
- * @param argc 
- * @param argv 
- * @return int 
+ *
+ * @param argc
+ * @param argv
+ * @return int
  */
-int main(int argc, char const *argv[])
+int
+main (int argc, char const *argv[])
 {
-    
-    return EXIT_SUCCESS;
+
+  return EXIT_SUCCESS;
 }
