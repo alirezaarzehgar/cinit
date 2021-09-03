b693398a841249751701bece83ca8369812eb26f
alirezaarzehgar <email@alirezaarzehgar.com>
2021-09-04 02:24:39 +0430

    implement udp server

--- samples/client-server/udp/common/common.h
+++ samples/client-server/udp/common/common.h
@@ -5,5 +5,13 @@
 #include <stdlib.h>
 #include <unistd.h>
 
+#include <string.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+
+#define PORT    1234
+#define IP      "127.0.0.1"
 
 #endif // COMMON_H
--- samples/client-server/udp/server/main.c
+++ samples/client-server/udp/server/main.c
@@ -21,6 +21,57 @@
 int
 main (int argc, char const *argv[])
 {
+  int fd;
+
+  struct sockaddr_in saddr;
+
+  int retval;
+
+  /* create socket */
+  fd = socket (AF_INET, SOCK_DGRAM, IPPROTO_UDP);
+
+  if (fd == -1)
+    {
+      fprintf (stderr, "Couldn't create socket\n");
+      exit (EXIT_FAILURE);
+    }
+
+  /* binding */
+  saddr.sin_family = AF_INET;
+
+  saddr.sin_port = htons (PORT);
+
+  saddr.sin_addr.s_addr = inet_addr (IP);
+
+  retval = bind (fd, (struct sockaddr *)&saddr, sizeof (saddr));
+
+  if (retval == -1)
+    {
+      fprintf (stderr, "Couldn't bind on network\n");
+      exit (EXIT_FAILURE);
+    }
+
+  /* loop */
+  while (1)
+    {
+      int counter = 0;
+
+      struct sockaddr_in caddr;
+
+      socklen_t caddrLen = sizeof (saddr);
+
+      char buf[BUFSIZ];
+
+      while (counter <= 0)
+        {
+          counter = recvfrom (fd, buf, BUFSIZ, 0, (struct sockaddr *)&caddr,
+                              &caddrLen);
+        }
+
+      strcpy (buf, "Hello World from server\n");
+
+      sendto (fd, buf, strlen (buf), 0, (struct sockaddr *)&caddr, caddrLen);
+    }
 
   return EXIT_SUCCESS;
 }
